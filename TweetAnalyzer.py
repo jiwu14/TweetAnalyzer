84fe2d76df921f26f40840d740a314cc5b4b9bd4
Sam Wu <sjiwu14@gmail.com>
2015-12-06 14:20:20 -0800

    Tried a few things but meh

--- TweetAnalyzer.py
+++ TweetAnalyzer.py
@@ -10,6 +10,7 @@ from scipy.sparse import csr_matrix
 from sklearn.metrics.pairwise import cosine_distances
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score
 
 def preprocess_tweets(tweets):
     stemmer = SnowballStemmer("english")
@@ -41,10 +42,10 @@ def get_tfidf_vectors(tweets):
     vectorizer = TfidfVectorizer()
     return vectorizer.fit_transform(tweets)
 
-def cluster_DBSCAN(sparse_dist_matrix, minPts, eps):
-    #clusterer = DBSCAN(eps=eps, min_samples=minPts, metric="cosine", algorithm="brute")
-    clusterer = DBSCAN(eps=eps, min_samples=minPts, metric="precomputed", algorithm="brute")
-    return clusterer.fit_predict(sparse_dist_matrix)
+def cluster_DBSCAN(data, minPts, eps):
+    clusterer = DBSCAN(eps=eps, min_samples=minPts, metric="cosine", algorithm="brute")
+    #clusterer = DBSCAN(eps=eps, min_samples=minPts, metric="precomputed", algorithm="brute")
+    return clusterer.fit_predict(data)
 
 def main():
     if len(sys.argv) == 4:
@@ -60,17 +61,25 @@ def main():
             print("Preprocessed data")
             tweets_tfidf_matrix = get_tfidf_vectors(tweets_preprocessed)
             print("Tfidf vectors generated")
-            tweets_sparse_dist_matrix = get_sparse_dist_matrix(tweets_tfidf_matrix, eps)
-            print("Sparse distance matrix generated")
-            cluster_labels = cluster_DBSCAN(tweets_sparse_dist_matrix, minPts, eps)
+            #tweets_sparse_dist_matrix = get_sparse_dist_matrix(tweets_tfidf_matrix, eps)
+            #print("Sparse distance matrix generated")
+            cluster_labels = cluster_DBSCAN(tweets_tfidf_matrix, minPts, eps)
             print("Clustered with DBSCAN")
 
             nclusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)
             print("%d clusters present in dataset" % nclusters)
 
-            for cluster_label, cluster_tweets in itertools.groupby(
-                    sorted(zip(tweets, cluster_labels), key=itemgetter(1)),
-                    lambda x: x[1]):
+            #s_score = silhouette_score(tweets_tfidf_matrix, cluster_labels, metric="cosine")
+            #print("silhouette score: %f" % s_score)
+
+            clustered_tweets_grouped = ((cluster, list(cluster_t))
+                                        for cluster, cluster_t in
+                                            itertools.groupby(sorted(
+                                                                zip(tweets, cluster_labels),
+                                                                key=itemgetter(1)),
+                                                            lambda x: x[1]))
+
+            for cluster_label, cluster_tweets in itertools.islice(clustered_tweets_grouped, 1, None):
                 print("\nCluster", "Noise" if cluster_label == -1 else cluster_label)
                 for cluster_tweet in cluster_tweets:
                     print(cluster_tweet[0]["text"])
